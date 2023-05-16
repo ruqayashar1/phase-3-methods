728421c18b7040053e6769327dc0edae1e919f0d
rukia <ruqayashar1@gmail.com>
2023-05-16 08:55:04 +0300

    ruby methods

---
+++ js/methods.rb
@@ -0,0 +1,10 @@
+def my_method(param)
+  puts "Running my_method"
+  param + 1
+end
+
+my_method_return_value = my_method(1)
+# Running my_method
+# => 2
+my_method_return_value
+# => 2
--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!" 
+end
+def greet name
+    puts "Hello, #{name}!"
+end
+greet ("Rukia")
+
+def greet_with_default(name = "programmer!")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default()
+ 
+def add(num1, num2)
+    num1 + num2
+end
+result = add(2,5)
+puts result
+
+def halve(num)
+    return num / 2 if num.is_a?(Integer)
+    return nil
+  end
+  result = halve(6)
+  puts result
+  result = halve(5.5)
+  puts result
+
